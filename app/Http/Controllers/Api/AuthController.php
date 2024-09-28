9067b972b4ee80330dec12ed509cbafb7089eacb
AlvinSanudharma <alvinsanudharma@gmail.com>
2024-09-28 10:06:12 +0800

    Create Helper

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -85,6 +85,11 @@ class AuthController extends Controller
         }
     }
 
+    public function login(Request $request) 
+    {
+        $credentials = $request->only('email', 'password');
+    }
+
     private function generatedCardNumber($length)
     {
         $result = '';
