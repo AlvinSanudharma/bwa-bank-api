87785b2c6be91b97073eb4aa835b806b82e8ec1c
AlvinSanudharma <alvinsanudharma@gmail.com>
2024-10-07 20:14:32 +0800

    Middleware

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/test', function (Request $request) {
+//     echo('success');
+// })->middleware('jwt.verify');
 
 // NOTE: Auth routes
 Route::post('register', [AuthController::class, 'register']);
