191f2ffd676d8b4a494960883dcb06242e1fdf78
AlvinSanudharma <alvinsanudharma@gmail.com>
2025-07-02 13:27:21 +0800

    Dataplan Part 2

--- app/Http/Controllers/Api/DataPlanController.php
+++ app/Http/Controllers/Api/DataPlanController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\DataPlan;
+use App\Models\DataPlanHistory;
 use App\Models\PaymentMethod;
+use App\Models\Transaction;
 use App\Models\TransactionType;
 use App\Models\Wallet;
 use Illuminate\Http\Request;
@@ -58,6 +60,38 @@ class DataPlanController extends Controller
             ],400); 
         }
 
-        return true;
+        DB::beginTransaction();
+
+        try {
+            $transaction = Transaction::create([
+                'user_id' => $userId,
+                'transaction_type_id' => $transactionType->id,
+                'payment_method_id' => $paymentMethod->id,
+                'amount' => $dataPlan->price,
+                'transaction_code' => strtoupper(Str::random(10)),
+                'description' => 'Internet Data Plan '.$dataPlan->name,
+                'status' => 'success'
+            ]);
+
+            DataPlanHistory::create([
+                'data_plan_id' => $request->data_plan_id,
+                'transaction_id' => $transaction->id,
+                'phone_number' => $request->phone_number
+            ]);
+
+            $userWallet->decrement('balance', $dataPlan->price);
+
+            DB::commit();
+
+            return response()->json([
+                'message' => 'Buy Data Plan Success'
+            ]);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+
+            return response()->json([
+                'message' => $th->getMessage()
+            ], 500);
+        }
     }
 }
